130028f86878a6a28516ab1335c2b20dc9ae8fae
Ali Malik <ali.m.malik@gmail.com>
2025-07-02 17:26:40 -0400

    Fix install and script bugs

--- scripts/service.sh
+++ scripts/service.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Resolve service account and password
-$RESOLVE_USER="$1"
-$RESOLVE_PASS="$2"
+RESOLVE_USER="$1"
+RESOLVE_PASS="$2"
 
 # Create account
 useradd -c "Resolve Service" -m "$RESOLVE_USER"
